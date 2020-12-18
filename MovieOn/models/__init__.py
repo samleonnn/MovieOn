b726870f18acf0ff0e7ff43febef1906c69bad6e
samleonnn <samueldharmowongso45@gmail.com>
2020-12-18 18:39:32 +0700

    bug fixing on models and controller #1

--- MovieOn/models/__init__.py
+++ MovieOn/models/__init__.py
@@ -2,7 +2,4 @@ from .movie import Movie
 from .director import Director
 from .genre import Genre
 from .cast import Cast
-from .age_rate import Age_rate
-from .imdb import IMDBID
-from .country import Country
 from .ratings import Rating
--- MovieOn/models/movie.py
+++ MovieOn/models/movie.py
@@ -1,16 +1,13 @@
 from django.db import models
 from MovieOn.models.genre import Genre
 from MovieOn.models.cast import Cast
-from MovieOn.models.imdb import IMDBID
-from MovieOn.models.country import Country
 from MovieOn.models.ratings import Rating
-from MovieOn.models.age_rate import Age_rate
 from MovieOn.models.director import Director
 
 class Movie(models.Model):
     title = models.CharField(max_length = 200)
     year = models.CharField(max_length = 5)
-    rated = models.ForeignKey(Age_rate, null=True, on_delete=models.SET_NULL)
+    rated = models.CharField(max_length=10)
     datepublish = models.CharField(max_length = 100)
     runtime = models.CharField(max_length=25)
     genre = models.ManyToManyField(Genre)
@@ -18,12 +15,11 @@ class Movie(models.Model):
     writer = models.CharField(max_length=1000)
     cast = models.ManyToManyField(Cast)
     synopsis = models.CharField(max_length=1000)
-    country = models.ManyToManyField(Country)
+    country = models.CharField(max_length=100)
     poster = models.URLField()
     ratings = models.ManyToManyField(Rating)
-    imdbID = models.OneToOneField(IMDBID, on_delete=models.CASCADE)
+    imdbID = models.CharField(max_length=30)
     type = models.CharField(max_length=10)
-    totalseasons = models.CharField(max_length=4, null=True)
 
     class Meta:
         app_label = 'MovieOn'
