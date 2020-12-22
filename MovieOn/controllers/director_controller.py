f99634858c8ca89e4f73ad1de6c93a13bad1734a
samleonnn <samueldharmowongso45@gmail.com>
2020-12-22 14:14:35 +0700

    update: sort by genre

--- MovieOn/controllers/director_controller.py
+++ MovieOn/controllers/director_controller.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from MovieOn.models.director import Director
+from MovieOn.models.genre import Genre
 from django.forms.models import model_to_dict
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
@@ -8,9 +9,11 @@ from django.contrib.auth.decorators import login_required
 from MovieOn.forms import DirectorForm
 
 def index(request):
+    genre_data = Genre.objects.all()
     directors = Director.objects.all()
     context = {
         'directors': directors,
+        'genre_data': genre_data,
     }
 
     return render(request, 'director/index.html', context=context)
